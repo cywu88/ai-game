34e979bb731646be5627a94dd418a25e5e633d26
wucy <cywu88@126.com>
2016-07-02 00:12:09 +0800

    first commit

---
+++ WestWorld/WestWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WestWorld
+//
+//  Created by wucy on 16/7/2.
+//  Copyright © 2016年 wucy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

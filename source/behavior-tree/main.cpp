a043b55005f00fd65540003d1174a1dce7a67cc5
吴存勇 <cywu88@126.com>
2016-07-05 15:08:45 +0800

    win32工程修改

--- source/behavior-tree/main.cpp
+++ source/behavior-tree/main.cpp
@@ -3,178 +3,299 @@
 
 #include "stdafx.h"
 #include "main.h"
+#include "constants.h"
 
-#define MAX_LOADSTRING 100
+LPCWSTR g_szApplicationName = L"Steering Behaviors - Another Big Shoal";
+LPCWSTR	g_szWindowClassName = L"MyWindowClass";
 
-// 全局变量: 
-HINSTANCE hInst;                                // 当前实例
-WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
-WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
-
-// 此代码模块中包含的函数的前向声明: 
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+//GameWorld* g_GameWorld;
+LRESULT CALLBACK WindowProc(HWND   hwnd,
+	UINT   msg,
+	WPARAM wParam,
+	LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	//these hold the dimensions of the client window area
+	static int cxClient, cyClient;
 
-    // TODO: 在此放置代码。
+	//used to create the back buffer
+	static HDC		hdcBackBuffer;
+	static HBITMAP	hBitmap;
+	static HBITMAP	hOldBitmap;
 
-    // 初始化全局字符串
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_BEHAVIORTREE, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	switch (msg)
+	{
 
-    // 执行应用程序初始化: 
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+		//A WM_CREATE msg is sent when your application window is first
+		//created
+	case WM_CREATE:
+	{
+		//to get get the size of the client window first we need  to create
+		//a RECT and then ask Windows to fill in our RECT structure with
+		//the client window size. Then we assign to cxClient and cyClient 
+		//accordingly
+		RECT rect;
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_BEHAVIORTREE));
+		GetClientRect(hwnd, &rect);
 
-    MSG msg;
+		cxClient = rect.right;
+		cyClient = rect.bottom;
 
-    // 主消息循环: 
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+		//seed random number generator
+		//srand((unsigned)time(NULL));
 
-    return (int) msg.wParam;
+
+		//---------------create a surface to render to(backbuffer)
+
+		//create a memory device context
+		hdcBackBuffer = CreateCompatibleDC(NULL);
+
+		//get the DC for the front buffer
+		HDC hdc = GetDC(hwnd);
+
+		hBitmap = CreateCompatibleBitmap(hdc,
+			cxClient,
+			cyClient);
+
+
+		//select the bitmap into the memory device context
+		hOldBitmap = (HBITMAP)SelectObject(hdcBackBuffer, hBitmap);
+
+		//don't forget to release the DC
+		ReleaseDC(hwnd, hdc);
+
+		/*g_GameWorld = new GameWorld(cxClient, cyClient);
+
+		ChangeMenuState(hwnd, IDR_PRIORITIZED, MFS_CHECKED);
+		ChangeMenuState(hwnd, ID_VIEW_FPS, MFS_CHECKED);*/
+
+	}
+
+	break;
+
+	case WM_COMMAND:
+	{
+		//g_GameWorld->HandleMenuItems(wParam, hwnd);
+	}
+
+	break;
+
+
+	case WM_LBUTTONUP:
+	{
+		//g_GameWorld->SetCrosshair(MAKEPOINTS(lParam));
+	}
+
+	break;
+
+	case WM_KEYUP:
+	{
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+		{
+			SendMessage(hwnd, WM_DESTROY, NULL, NULL);
+		}
+
+		break;
+
+		case 'R':
+		{
+			//delete g_GameWorld;
+
+			//g_GameWorld = new GameWorld(cxClient, cyClient);
+		}
+
+		break;
+
+
+		}//end switch
+
+		 //handle any others
+		//g_GameWorld->HandleKeyPresses(wParam);
+
+	}//end WM_KEYUP
+
+	break;
+
+
+	case WM_PAINT:
+	{
+
+		PAINTSTRUCT ps;
+
+		BeginPaint(hwnd, &ps);
+
+		//fill our backbuffer with white
+		BitBlt(hdcBackBuffer,
+			0,
+			0,
+			cxClient,
+			cyClient,
+			NULL,
+			NULL,
+			NULL,
+			WHITENESS);
+
+
+		//gdi->StartDrawing(hdcBackBuffer);
+
+		//g_GameWorld->Render();
+
+		//gdi->StopDrawing(hdcBackBuffer);
+
+
+
+		//now blit backbuffer to front
+		BitBlt(ps.hdc, 0, 0, cxClient, cyClient, hdcBackBuffer, 0, 0, SRCCOPY);
+
+		EndPaint(hwnd, &ps);
+
+	}
+
+	break;
+
+
+
+	case WM_DESTROY:
+	{
+
+		//clean up our backbuffer objects
+		SelectObject(hdcBackBuffer, hOldBitmap);
+
+		DeleteDC(hdcBackBuffer);
+		DeleteObject(hBitmap);
+
+
+
+		// kill the application, this sends a WM_QUIT message  
+		PostQuitMessage(0);
+	}
+
+	break;
+
+	}//end switch
+
+	 //this is where all the messages not specifically handled by our 
+	 //winproc are sent to be processed
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+//-------------------------------- WinMain -------------------------------
+//
+//	The entry point of the windows program
+//------------------------------------------------------------------------
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     szCmdLine,
+	int       iCmdShow)
+{
+	//handle to our window
+	HWND						hWnd;
+
+	//our window class structure
+	WNDCLASSEX     winclass;
+
+	// first fill in the window class stucture
+	winclass.cbSize = sizeof(WNDCLASSEX);
+	winclass.style = CS_HREDRAW | CS_VREDRAW;
+	winclass.lpfnWndProc = WindowProc;
+	winclass.cbClsExtra = 0;
+	winclass.cbWndExtra = 0;
+	winclass.hInstance = hInstance;
+	winclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	winclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	winclass.hbrBackground = NULL;
+	winclass.lpszMenuName = MAKEINTRESOURCEW(IDC_BEHAVIORTREE);
+	winclass.lpszClassName = g_szWindowClassName;
+	winclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	//register the window class
+	if (!RegisterClassEx(&winclass))
+	{
+		MessageBox(NULL, L"Registration Failed!", L"Error", 0);
+
+		//exit the application
+		return 0;
+	}
+
+	//create the window and assign its ID to hwnd    
+	hWnd = CreateWindowEx(NULL,                 // extended style
+		g_szWindowClassName,  // window class name
+		g_szApplicationName,  // window caption
+		WS_OVERLAPPED | WS_VISIBLE | WS_CAPTION | WS_SYSMENU,
+		GetSystemMetrics(SM_CXSCREEN) / 2 - constWindowWidth / 2,
+		GetSystemMetrics(SM_CYSCREEN) / 2 - constWindowHeight / 2,
+		constWindowWidth,     // initial x size
+		constWindowHeight,    // initial y size
+		NULL,                 // parent window handle
+		NULL,                 // window menu handle
+		hInstance,            // program instance handle
+		NULL);                // creation parameters
+
+							  //make sure the window creation has gone OK
+	if (!hWnd)
+	{
+		MessageBox(NULL, L"CreateWindowEx Failed!", L"Error!", 0);
+	}
+
+
+	//make the window visible
+	ShowWindow(hWnd, iCmdShow);
+	UpdateWindow(hWnd);
+
+	// Enter the message loop
+	bool bDone = false;
+
+	//create a timer
+	//PrecisionTimer timer;
+
+	//timer.SmoothUpdatesOn();
+
+	////start the timer
+	//timer.Start();
+
+	MSG msg;
+
+	while (!bDone)
+	{
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				//stop loop if it's a quit message
+				bDone = true;
+			}
+
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+
+		if (msg.message != WM_QUIT)
+		{
+			//update
+			//g_GameWorld->Update(timer.TimeElapsed());
+
+			//render
+			//RedrawWindow(hWnd, false);
+
+			Sleep(2);
+		}
+
+	}//end while
+
+
+
+
+	//delete g_GameWorld;
+
+	//UnregisterClass(g_szWindowClassName, winclass.hInstance);
+
+	return msg.wParam;
 }
 
 
 
-//
-//  函数: MyRegisterClass()
-//
-//  目的: 注册窗口类。
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_BEHAVIORTREE));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_BEHAVIORTREE);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
-}
-
-//
-//   函数: InitInstance(HINSTANCE, int)
-//
-//   目的: 保存实例句柄并创建主窗口
-//
-//   注释: 
-//
-//        在此函数中，我们在全局变量中保存实例句柄并
-//        创建和显示主程序窗口。
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // 将实例句柄存储在全局变量中
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  目的:    处理主窗口的消息。
-//
-//  WM_COMMAND  - 处理应用程序菜单
-//  WM_PAINT    - 绘制主窗口
-//  WM_DESTROY  - 发送退出消息并返回
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 分析菜单选择: 
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 在此处添加使用 hdc 的任何绘图代码...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
-// “关于”框的消息处理程序。
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
-}
+ 
